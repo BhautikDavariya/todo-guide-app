0ee31a70bc0fdd8809932c9e6704bfde0af2b1a2
bhautik davariya <bhautikmerndeveloprs@gmail.com>
2024-04-30 20:49:20 +0530

    build up

---
+++ out/_next/static/SMjtTvnJMxwq1puJIIsKZ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/88-b8f909616ee3e4a6.js","static/chunks/pages/index-a8143b91c1b04a1a.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
