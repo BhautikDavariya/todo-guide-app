5e3ebc55882ea1c55016e057f956eb95fc1aabdd
bhautik davariya <bhautikmerndeveloprs@gmail.com>
2024-04-30 20:57:26 +0530

    git config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
+  // Add basePath
+  basePath: '/todo-guide-app',
 }
 
 module.exports = nextConfig
